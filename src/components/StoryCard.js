6f6becaa0bc8d6b6f139db9a8bcd0504b4105bd1
katycarr <khcarr9@gmail.com>
2018-03-23 14:24:06 -0400

    added to card component

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import { Card, Icon, Comment } from 'semantic-ui-react'
 
 export default class StoryCard extends Component {
 	render() {
-		return <div>hi from storycard</div>;
+		console.log(this.props.story)
+		const comments = this.props.story.comments.map(comment => {
+			return (
+				<Comment>
+					<Comment.Content>
+						<Comment.Author></Comment.Author>
+						<Comment.Text>{comment.content}</Comment.Text>
+					</Comment.Content>
+				</Comment>
+			)
+		})
+		console.log(comments)
+		return (
+			<div>
+				<Card>
+					<Card.Content>
+						<Card.Description>{this.props.story.content}</Card.Description>
+						<Card.Meta> <Icon name='user' />{this.props.story.user.username}</Card.Meta>
+					</Card.Content>
+				</Card>
+				<Comment.Group>
+					{comments}
+				</Comment.Group>
+			</div>
+		)
 	}
 }
