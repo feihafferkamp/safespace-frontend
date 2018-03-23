01936bd50f1890ca5331b77fb060fff20c6a87b0
feihafferkamp <fei.hafferkamp@gmail.com>
2018-03-23 15:01:33 -0400

    add comments show and form

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,32 +1,67 @@
 import React, { Component } from 'react';
-import { Card, Icon, Comment } from 'semantic-ui-react'
+import { Card, Icon, Comment, Form, Button } from 'semantic-ui-react';
+import CommentShow from './CommentShow';
 
 export default class StoryCard extends Component {
+	state = { comment: { username: '', content: '' } };
+
+	handleInputChange = e => {
+		this.setState({
+			comment: { ...this.state.comment, [e.target.name]: e.target.value }
+		});
+	};
+
+	handleCommentSubmit = e => {
+		e.preventDefault();
+		this.postComment();
+	};
+
+	postComment = () => {
+		let options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify({})
+		};
+	};
+
 	render() {
-		console.log(this.props.story)
 		const comments = this.props.story.comments.map(comment => {
-			return (
-				<Comment>
-					<Comment.Content>
-						<Comment.Author></Comment.Author>
-						<Comment.Text>{comment.content}</Comment.Text>
-					</Comment.Content>
-				</Comment>
-			)
-		})
-		console.log(comments)
+			return <CommentShow comment={comment} key={comment.id} />;
+		});
+
 		return (
 			<div>
 				<Card>
 					<Card.Content>
 						<Card.Description>{this.props.story.content}</Card.Description>
-						<Card.Meta> <Icon name='user' />{this.props.story.user.username}</Card.Meta>
+						<Card.Meta>
+							<Icon name="user" />
+							{this.props.story.user.username}
+						</Card.Meta>
 					</Card.Content>
 				</Card>
 				<Comment.Group>
 					{comments}
+					<Form onSubmit={this.handleCommentSubmit}>
+						<Form.TextArea
+							name="content"
+							value={this.state.newComment}
+							onChange={this.handleCommentChange}
+						/>
+						<Form.Field>
+							<input
+								onChange={this.handleInputChange}
+								name="username"
+								placeholder="Your Username"
+							/>
+						</Form.Field>
+						<Button type="submit" content="Submit" />
+					</Form>
 				</Comment.Group>
 			</div>
-		)
+		);
 	}
 }
