546c03cb3a2c3304f3a1c7f15750c368c7762ff2
f3igao <f3igao@icloud.com>
2018-03-23 17:31:56 -0400

    refactor comment container and add instant re-render

---
+++ src/components/CommentForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+
+export default class CommentForm extends Component {
+	state = { comment: { username: '', content: '' } };
+
+	handleInputChange = e => {
+		this.setState({
+			comment: { ...this.state.comment, [e.target.name]: e.target.value }
+		});
+	};
+
+	handleCommentSubmit = e => {
+		e.preventDefault();
+		this.props.submitComment(this.state);
+		this.setState({ comment: { username: '', content: '' } });
+	};
+
+	render() {
+		return (
+			<Form onSubmit={this.handleCommentSubmit}>
+				<Form.TextArea
+					name="content"
+					value={this.state.comment.content}
+					placeholder="Your comment here..."
+					onChange={this.handleInputChange}
+				/>
+				<Form.Field>
+					<input
+						name="username"
+						value={this.state.comment.username}
+						placeholder="Your Username"
+						onChange={this.handleInputChange}
+					/>
+				</Form.Field>
+				<Button type="submit" content="Submit" />
+			</Form>
+		);
+	}
+}
