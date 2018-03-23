fb01f8dce3c1f0e6b1b85c8d2cc8525b805388f3
katycarr <khcarr9@gmail.com>
2018-03-23 14:24:06 -0400

    added to card component

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import StoryCard from './StoryCard';
+import { Card } from 'semantic-ui-react'
 
 export default class StoryList extends Component {
 	render() {
 		const storyCards = this.props.stories.map(s => (
 			<StoryCard story={s} key={s.id} />
 		));
-		return <div>{storyCards}</div>;
+		return (
+			<Card.Group>{storyCards}</Card.Group>
+		)
 	}
 }
