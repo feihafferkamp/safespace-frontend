fcc6813ef1a987704b490402958e107dbf887a17
katycarr <khcarr9@gmail.com>
2018-03-24 17:25:41 -0400

    new story form

--- src/containers/NewStoryContainer.js
+++ src/containers/NewStoryContainer.js
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 import NewStoryForm from '../components/NewStoryForm';
 
 export default class NewStoryContainer extends Component {
+	state = {
+		tags:[]
+	}
+
+	componentDidMount = () => {
+		fetch('http://localhost:3000/tags')
+			.then(res => res.json())
+			.then(tags => this.setState({tags}))
+	}
+
+	addStory = storyAttributes => {
+		let options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify({story: storyAttributes})
+		}
+		fetch('http://localhost:3000/stories', options)
+			.then(res => res.json())
+			.then(json => console.log(json))
+	}
+
 	render() {
-		return <NewStoryForm />;
+		return <NewStoryForm tags={this.state.tags} handleSubmit={this.addStory}/>;
 	}
 }
