b2143ec56bba45ca23c9f3616546acdb2ad89e9d
katycarr <khcarr9@gmail.com>
2018-03-24 17:25:41 -0400

    new story form

--- src/components/NewStoryForm.js
+++ src/components/NewStoryForm.js
@@ -1,7 +1,81 @@
 import React, { Component } from 'react';
+import NewTagForm from './NewTagForm'
+import { Form, Input, TextArea, Button, Segment } from 'semantic-ui-react'
 
 export default class NewStoryForm extends Component {
+	state = {
+		username:'',
+		content:'',
+		tags:[],
+		user_id:'',
+	}
+
+	handleChange = e => {
+		this.setState({
+			[e.target.name]:e.target.value
+		})
+	}
+
+	handleSubmit = e => {
+		e.preventDefault()
+		const storiesTags = this.state.tags.map(tag => {
+			return {tag_attributes: tag}
+		})
+		let story = {
+			user_id: this.state.user_id,
+			content: this.state.content,
+			stories_tags_attributes: storiesTags
+		}
+		this.props.handleSubmit(story)
+	}
+
+	addTag = newTag => {
+		const tag = {name: newTag}
+		this.setState({
+			tags: [...this.state.tags, tag]
+		})
+	}
+
+	checkUser = () => {
+		fetch(`http://localhost:3000/users?name=${this.state.username}`)
+			.then(res => res.json())
+			.then(json => {
+				if(json) {
+					this.setState({
+						user_id:json.id
+					})
+				}
+			})
+	}
+
 	render() {
-		return <div> hi from new story form</div>;
+		const tagItems = this.state.tags.map(tag => {
+			return <li key={tag.name}>{tag.name}</li>
+		})
+
+		return (
+			<div>
+				<Form onSubmit={this.handleSubmit} id='storyForm'>
+					<Form.Field inline>
+						<label>Username</label>
+						<Input name='username' onBlur={this.checkUser} value={this.state.username} onChange={this.handleChange} />
+					</Form.Field>
+					<Form.Field>
+						<label>Story</label>
+						<TextArea name='content' value={this.state.content} onChange={this.handleChange} />
+					</Form.Field>
+				</Form>
+				<Segment>
+					<ul>
+						{tagItems}
+					</ul>
+				</Segment>
+
+				<NewTagForm handleSubmit={this.addTag} />
+
+				<Button type='submit' form='storyForm'>Submit</Button>
+			</div>
+
+		)
 	}
 }
---
+++ src/components/NewTagForm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Form, Input, Button } from 'semantic-ui-react'
+
+
+class NewTagForm extends React.Component {
+  state = {
+    value:''
+  }
+
+
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.handleSubmit(this.state.value)
+    this.setState({
+      value:''
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  render() {
+
+    return(
+      <Form id='tagForm' onSubmit={this.onSubmit}>
+        <label>Tags: </label>
+        <Input name='tag' value={this.state.value} onChange={this.handleChange}/>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    )
+  }
+
+}
+
+export default NewTagForm
