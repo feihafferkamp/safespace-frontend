4ffacabd59a5cc2274757c652de9c0d98c590043
katycarr <khcarr9@gmail.com>
2018-03-27 09:39:21 -0400

    started feed

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import { Card, Icon } from 'semantic-ui-react';
-import CommentContainer from '../containers/CommentContainer';
+import { Card, Icon, Comment, Form, Button } from 'semantic-ui-react';
+import CommentContainer from '../containers/CommentContainer'
 
 export default class StoryCard extends Component {
+
+
+
 	render() {
+
+
 		return (
-			<Card>
-				<Card.Content>
-					<Card.Description>{this.props.story.content}</Card.Description>
-					<Card.Meta>
-						<Icon name="user" />
-						{this.props.story.user.username}
-					</Card.Meta>
-				</Card.Content>
-				<Card.Content extra>
-					<CommentContainer
-						comments={this.props.story.comments}
-						storyId={this.props.story.id}
-					/>
-				</Card.Content>
-			</Card>
+			<div>
+				<Card>
+					<Card.Content>
+						<Card.Description>{this.props.story.content}</Card.Description>
+						<Card.Meta>
+							<Icon name="user" />
+							{this.props.story.user.username}
+						</Card.Meta>
+					</Card.Content>
+				</Card>
+				<CommentContainer comments={this.props.story.comments} storyId={this.props.story.id}/>
+			</div>
 		);
 	}
 }
---
+++ src/components/StoryItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Card, Icon, Label, Container, List, Segment } from 'semantic-ui-react';
+import CommentContainer from '../containers/CommentContainer';
+
+
+export default class StoryItem extends Component {
+	render() {
+		const tags = this.props.story.tags.map(tag => {
+			return <Label key={tag.id}>{tag.name}</Label>
+		})
+		return (
+			<div className='story-item'>
+				<Segment piled>
+						<div>{this.props.story.content}</div>
+						<div>
+							<Icon name="user" />
+							{this.props.story.user.username}
+						</div>
+						<CommentContainer
+							comments={this.props.story.comments}
+							storyId={this.props.story.id}
+						/>
+          <Label.Group tag color='teal'>
+              {tags}
+            </Label.Group>
+				</Segment>
+			</div>
+		);
+	}
+}
