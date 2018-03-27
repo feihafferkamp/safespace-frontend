b541c7a032f94c5e0cda0f061e4ee88a6912cf02
katycarr <khcarr9@gmail.com>
2018-03-27 11:07:18 -0400

    refactored auth

---
+++ src/components/withAuthentication.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function withAuthentication(ComponentWePassedIn, user) {
+
+  return (class extends React.Component {
+
+      constructor(props){
+        super(props);
+      }
+
+
+      componentDidMount() {
+        this._checkAndRedirect();
+      }
+
+      componentDidUpdate() {
+        this._checkAndRedirect();
+      }
+
+      _checkAndRedirect(){
+        if (!localStorage.getItem("jwt"))
+          this.props.history.push("/")
+      }
+
+      render() {
+        console.log(user)
+        return <ComponentWePassedIn user={user} {...this.props} />
+      }
+    })
+
+
+
+};
