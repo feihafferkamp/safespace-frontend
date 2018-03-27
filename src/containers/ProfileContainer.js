282e23ce3d25e983c5039059e97f6d605b55e439
katycarr <khcarr9@gmail.com>
2018-03-27 15:27:21 -0400

    profile

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
+import StoryItem from '../components/StoryItem'
 
 export default class ProfileContainer extends React.Component {
   state = {
@@ -7,28 +8,38 @@ export default class ProfileContainer extends React.Component {
   }
 
   componentDidMount() {
-    const id = this.props.user.id
-    let options = {
-      method: "GET",
-      headers: {
-        "Content-Type":"application/json",
-        Accept: "application/json",
-        Authorization: `Bearer ${localStorage.getItem('jwt')}`
+    if (this.props.user) {
+      const id = this.props.user.id
+      let options = {
+        method: "GET",
+        headers: {
+          "Content-Type":"application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem('jwt')}`
+        }
       }
+      fetch("http://localhost:3000/users/"+id, options)
+        .then((res) => res.json())
+        .then((json) => {
+          this.setState({
+            myStories:json.stories
+          })
+      })
     }
-    fetch("http://localhost:3000/users/"+id, options)
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json)
+  }
+
+  storyCards = () => {
+    return this.state.myStories.map(story => {
+      return <StoryItem story={story} user={this.props.user} key={story.id} />
     })
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.state.stories)
     return(
       <Grid cols={2}>
         <Grid.Column>
-
+          {this.storyCards()}
         </Grid.Column>
       </Grid>
     )
