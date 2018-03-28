5d61f0745864306322cbdfa99aeac98fdad199d8
f3igao <f3igao@icloud.com>
2018-03-28 14:43:36 -0400

    add disclarmer re: geolocation in about page

--- src/components/StaticComponent.js
+++ src/components/StaticComponent.js
@@ -11,6 +11,15 @@ const StaticComponent = props => {
 			<Segment raised padded basic>
 				Safe Space is an anonymous social network platform for women worldwide
 				to share their experiences, ask questions, and build community.
+				<p id="note">
+					<em>
+						Note: All location requests are for the sole purpose of placing your
+						stories on the map. We believe seeing posts by location can add a
+						sense of solidarity to the Safe Space community. If you choose to
+						block the geolocation prompt, however, we comepletely understand and
+						will simply stash your story in the Pacific Ocean. :){' '}
+					</em>
+				</p>
 			</Segment>
 		</div>
 	);
@@ -19,7 +28,7 @@ const StaticComponent = props => {
 		<div className="static contact">
 			<h1>Contact</h1>
 			<Segment raised padded basic>
-				<p>Questions, comments, suggestions? Let us know!</p>
+				Questions, comments, suggestions? Let us know!
 				<p>
 					Katy: 123-456-7890
 					<br />Fei: 123-456-7890
