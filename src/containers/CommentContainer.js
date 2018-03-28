4126d241eba4a05d68c2dc7f31e2662a8216ef0d
katycarr <khcarr9@gmail.com>
2018-03-28 11:08:59 -0400

    edit stories

--- src/containers/CommentContainer.js
+++ src/containers/CommentContainer.js
@@ -56,7 +56,6 @@ export default class CommentContainer extends React.Component {
 		);
 
 	render() {
-		console.log(this.props)
 		return <Comment.Group>{this.generateDisplay()}</Comment.Group>;
 	}
 }
--- src/containers/EditStoryContainer.js
+++ src/containers/EditStoryContainer.js
@@ -1,18 +1,57 @@
 import React from 'react'
 import NewStoryForm from '../components/NewStoryForm'
+import '../stylesheets/new-story.css';
 
 export default class EditStoryContainer extends React.Component {
+  state = {
+    tags:[],
+    posted:false,
+    errors:''
+  }
 
+  componentDidMount = () => {
+    fetch('http://localhost:3000/tags')
+			.then(res => res.json())
+			.then(tags => this.setState({ tags }));
+  }
+
+  patchStory = storyAttributes => {
+		let options = {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('jwt')}`
+			},
+			body: JSON.stringify({ story: storyAttributes })
+		};
+		fetch('http://localhost:3000/stories/'+this.props.story.id, options)
+			.then(res => res.json())
+			.then(json => {
+				if (json.errors) {
+					this.setState({
+						posted: false,
+						errors: json.errors.join(', ')
+					});
+				} else {
+					this.setState({
+						posted: true
+					});
+				}
+			});
+      this.props.afterSubmit()
+	};
 
   render() {
     return(
-      <NewStoryForm
-        userId={this.state.user.id}
-        posted={this.state.posted}
-        errors={this.state.errors}
-        tags={this.state.tags}
-        handleSubmit={this.addStory}
-      />
+      <div className='container'>
+        <NewStoryForm
+          userId={this.props.user.id}
+          tags={this.state.tags}
+          handleSubmit={this.patchStory}
+          story={this.props.story}
+        />
+      </div>
     )
   }
 }
